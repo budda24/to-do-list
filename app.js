79fe1ca825e1889cd2ac136c15c8aba569eb37d9
Franek Jablonski <mimikkabehappy@gmail.com>
2020-12-26 08:25:52 +0100

    update js file

--- app.js
+++ app.js
@@ -1,76 +1,84 @@
 let items = [];
+let itemHTMLComponent;
+let dayDifference;
 
 function addItem() {
   let itemValue = document.getElementById("todo-input").value;
   items.push(itemValue);
   displayAllItems();
-  document.getElementById("todo-input").value="";
+  document.getElementById("todo-input").value = "";
 }
 let itemsDate = [];
-function addDate(){
-    let dateValue = document.getElementById("due-date").value;
-    itemsDate.push(dateValue);
-    displayAllItems();
-
+function addDate() {
+  let dateValue = document.getElementById("due-date").value;
+  itemsDate.push(dateValue);
+  displayAllItems();
 }
- 
+
 //have I do so many variables || is there a way to cut my code ??
 
 function displayAllItems() {
-    let itemContainerText = document.getElementById("todo-container");
-    itemContainerText.innerHTML = "";
+  let itemContainerText = document.getElementById("todo-container");
+  itemContainerText.innerHTML = "";
 
   for (let index = 0; index < items.length; index++) {
-    //add number of row 
+    //add number of row
     let htmlTrComponent = document.createElement("tr");
     let numberOfRow = document.createElement("td");
-    let textNumberRow = document.createTextNode(index +1);
+    let textNumberRow = document.createTextNode(index + 1);
     numberOfRow.appendChild(textNumberRow);
     htmlTrComponent.appendChild(numberOfRow);
     document.getElementById("todo-container").appendChild(htmlTrComponent);
     let item = items[index];
-    let itemDate =new Date(itemsDate[index]);
-    
+    let itemDate = new Date(itemsDate[index]);
+
     //creating text field
-    let itemHTMLComponent = document.createElement("td");
+    itemHTMLComponent = document.createElement("td");
     htmlTrComponent.appendChild(itemHTMLComponent);
     let listItemTextNode = document.createTextNode(item);
     itemHTMLComponent.appendChild(listItemTextNode);
 
     //calculate days beetwen
-    let currentDate = new Date();
-    let ddCurrent = currentDate.getTime();
-    let deadLine = itemDate;
-    let ddDedline = deadLine.getTime();
-    let dateDifference = ddDedline - ddCurrent;
-    let days = (1000*3600*24);
-    let dayDifference = Math.ceil(dateDifference / days);
+    dayDifference = calculateDaysBetween(itemDate);
     console.log(dayDifference);
 
     //checking if urgent
     itemHTMLComponent = document.createElement("td");
-    if(dayDifference <= 2)
-    {
-       //creat data field
-      htmlTrComponent.appendChild(itemHTMLComponent);
-      listItemTextNode = document.createTextNode(itemsDate[index]);
-      itemHTMLComponent.appendChild(listItemTextNode);
-      document.getElementById("due-date").value="";
+    setColoring(index, htmlTrComponent, itemsDate);
+  }
+}
 
-      //creating urgent
-      let textUrgent = document.createTextNode("URGENT"); 
-      let spamElement = document.createElement("spam");
-      spamElement.appendChild(textUrgent);
-      htmlTrComponent.appendChild(spamElement);
-      htmlTrComponent.classList.add("tableUrgent");
-    }
-    else{
-       //creat data field
-       htmlTrComponent.appendChild(itemHTMLComponent);
-       listItemTextNode = document.createTextNode(itemsDate[index]);
-       itemHTMLComponent.appendChild(listItemTextNode);
-       document.getElementById("due-date").value="";
-       htmlTrComponent.classList.add("tableNotUrgent");
-    }
+function calculateDaysBetween(itemDate) {
+  let currentDate = new Date();
+  let ddCurrent = currentDate.getTime();
+  let deadLine = itemDate;
+  let ddDedline = deadLine.getTime();
+  let dateDifference = ddDedline - ddCurrent;
+  let days = 1000 * 3600 * 24;
+  let dayDifference = Math.ceil(dateDifference / days);
+  return dayDifference;
+}
+
+function setColoring(index, htmlTrComponent, itemsDate) {
+  if (dayDifference <= 2) {
+    //creat data field
+    htmlTrComponent.appendChild(itemHTMLComponent);
+    listItemTextNode = document.createTextNode(itemsDate[index]);
+    itemHTMLComponent.appendChild(listItemTextNode);
+    document.getElementById("due-date").value = "";
+
+    //creating urgent
+    let textUrgent = document.createTextNode("URGENT");
+    let spamElement = document.createElement("spam");
+    spamElement.appendChild(textUrgent);
+    htmlTrComponent.appendChild(spamElement);
+    htmlTrComponent.classList.add("tableUrgent");
+  } else {
+    //creat data field
+    htmlTrComponent.appendChild(itemHTMLComponent);
+    listItemTextNode = document.createTextNode(itemsDate[index]);
+    itemHTMLComponent.appendChild(listItemTextNode);
+    document.getElementById("due-date").value = "";
+    htmlTrComponent.classList.add("tableNotUrgent");
   }
 }
